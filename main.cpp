0e314dcc396b4770901926342d123bef5a7210e5
alixsooahkim <99973140+alixsooahkim@users.noreply.github.com>
2022-04-15 17:19:26 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,16 +16,18 @@ void compInput (){
 //generate cards in Deck
 void generateDeck(int deck[])
 {
-    //fill in the 10 cards in the deck 
+    //fill in the 10 cards in the deck in order.
     for (int i = 0; i < 10; i++){
         deck[i] = i + 1;
     }
 
-    //shuffle the cards in deck
+    //shuffle the cards in deck.
     for (int i = 0; i < 9; i++)
     {
-        int j = rand() % 10;
-        //swap the two cards in deck
+        //create random index j to swap with i.
+        int j = rand() % 10; 
+        
+        //swap the two cards in deck.
         int temp = deck[j];
         deck[j] = deck[i];
         deck[i] = temp;
@@ -34,12 +36,14 @@ void generateDeck(int deck[])
 
 int main(){
     
+    //create random seed using time.
     srand(time(NULL));
-    //create 2 decks: userDeck and compDeck
+    
+    //create 2 decks: userDeck and compDeck.
     int userDeck[10];
     int compDeck[10];
     
-    //generate the 10 cards in each deck
+    //generate the 10 cards in each deck.
     generateDeck(userDeck);
     generateDeck(compDeck);
 
