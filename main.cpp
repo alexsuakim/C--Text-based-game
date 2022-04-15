e81c6b2050e38712b6800a91d1c66ab0bb3ad563
alixsooahkim <99973140+alixsooahkim@users.noreply.github.com>
2022-04-15 16:19:03 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,31 +6,31 @@
 using namespace std;
 
 void userInput (){
-   
+
 }
 
 void compInput (){
-  
+
 }
 
-int check_duplicate(deck[], i){
+int generate_card(int deck[], int i){
     srand((unsigned int) time(NULL));
     int card = rand() % 10 + 1;
     for (int j = 0; j < i - 1; j++){
         if (card == deck[j]){
-            card = rand() % 10 + 1;
-            check_duplicate(deck[], card);
+            generate_card(deck, card);
+        }
     }
     return card;
 }
 
-void random_generate(deck[]){
+void generate_deck(int deck[]){
     int card;
     srand((unsigned int) time(NULL));
     card = rand() % 10 + 1;
     deck[0] = card;
     for (int i = 1; i < 10; i++){
-        card = check_duplicate(deck[], i);
+        card = generate_card(deck, i);
         deck[i] = card;
     }
 }
@@ -38,8 +38,8 @@ void random_generate(deck[]){
 int main(){
     int userDeck[10];
     int compDeck[10];
-    random_generate(userDeck);
-    random_generate(compDeck);
+    generate_deck(userDeck);
+    generate_deck(compDeck);
 
     cout << "userDeck: " << endl;
     for (int i = 0; i < 10; i++){
@@ -52,5 +52,5 @@ int main(){
     }
     return 0;
 
-  
+
 }
