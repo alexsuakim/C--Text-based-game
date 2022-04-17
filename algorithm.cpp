fac87bf3077ec9ee623d99436b2cb31059a948e7
boyeongc <chboyeong@gmail.com>
2022-04-17 15:19:11 +0800

    Create algorithm.cpp

---
+++ algorithm.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+#include <random>
+#include <ctime>
+#include <iomanip>
+
+using namespace std;
+
+int algorithm (int userCard, int userBet) {
+    int compBet;
+    return compBet;
+}
